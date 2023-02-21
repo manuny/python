3c946c68404b91fbd4ef0c13021c6c834da0e5ee
Vasileva Masha <Vasilevamasha1982@gmail.com>
2023-02-22 02:12:10 +0300

    Задача4

--- seminar/4.py
+++ seminar/4.py
@@ -7,7 +7,7 @@
 
 year = int(input('Год:'))
 
-if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print('YES')
 else:
     print('NO')
